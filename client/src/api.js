293f3140595d0a07638be7f039ce5c45565fd91c
Dark-bit24 <linux7277@gmail.com>
2026-05-14 20:14:23 +0200

    replace localhost with render backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://learn-hub-x0ol.onrender.com"
+  baseURL: "https://learn-hub-x0ol.onrender.com/api"
 });
 
 export default API;
